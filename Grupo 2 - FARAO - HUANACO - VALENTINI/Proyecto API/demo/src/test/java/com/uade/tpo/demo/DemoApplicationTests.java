69cffb3a079567cf5758e87dbe2959621a7665fd
Alvaro-Huanaco-Llanque <227383127+Alvaro-Huanaco-Llanque@users.noreply.github.com>
2025-11-22 12:51:26 -0300

    Add H2 test dependency and test config

--- Grupo 2 - FARAO - HUANACO - VALENTINI/Proyecto API/demo/src/test/java/com/uade/tpo/demo/DemoApplicationTests.java
+++ Grupo 2 - FARAO - HUANACO - VALENTINI/Proyecto API/demo/src/test/java/com/uade/tpo/demo/DemoApplicationTests.java
@@ -2,8 +2,11 @@ package com.uade.tpo.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = Replace.ANY)
 class DemoApplicationTests {
 
 	@Test
